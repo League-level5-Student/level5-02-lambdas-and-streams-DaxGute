33872fafa3c2fe515615c1d55ea07d59079cdcfa
League Student <league@iMac-13.attlocal.net>
2020-01-12 12:56:32 -0800

    lmbdas make little to no sense but dats ok

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,9 +1,13 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class LambdaButtons {
 	private JFrame window = new JFrame();
@@ -17,6 +21,19 @@ public class LambdaButtons {
 		window.add(addNumbers);
 		window.add(randNumber);
 		window.add(tellAJoke);
+		addNumbers.addActionListener((e)->{
+			int num1 = Integer.parseInt(JOptionPane.showInputDialog("Enter Num: "));
+			int num2 = Integer.parseInt(JOptionPane.showInputDialog("Enter Num: "));
+			System.out.println(num1 + num2);
+		});
+		randNumber.addActionListener((e)->{
+			Random random = new Random();
+			int num = random.nextInt();
+			System.out.println(num);
+		});
+		tellAJoke.addActionListener((e)->{
+			System.out.println("no thanks");
+		});
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
@@ -28,6 +45,8 @@ public class LambdaButtons {
 	public static void main(String[] args) {
 		new LambdaButtons();
 	}
+
+
 	
 	
 }
