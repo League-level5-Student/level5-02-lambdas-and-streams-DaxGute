a3b3627b07b007e92214226f38260fd598d9bdd4
DaxGute <36718376+DaxGute@users.noreply.github.com>
2020-06-20 23:37:09 -0700

    done with level 5 module 2

--- src/_04_Searching_With_Streams/StreamSearcher.java
+++ src/_04_Searching_With_Streams/StreamSearcher.java
@@ -1,7 +1,9 @@
 package _04_Searching_With_Streams;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamSearcher {
@@ -10,12 +12,16 @@ public class StreamSearcher {
 	// use Streams to help us out.
 	public long containsCount(String[] strArr, String strToCount) {
 		//2. Convert the String array to a stream
-		
+		List<String> StringList = Arrays.asList(strArr);
+		List<String> totalMatches = StringList.stream().filter((e)->e==strToCount).collect(Collectors.toList());
 		//3. Use the filter method with a lambda to extract all the String
 		//   elements that match the String parameter.
-		
+		int total = 0;
+		for (String desiredString : totalMatches){
+			total ++;
+		}
 		//4. Return the count of the filtered stream.
 		
-		return 0;
+		return total;
 	}
 }
